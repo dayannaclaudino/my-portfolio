aa547ca9dbfeee317143932a7585da79f27594b6
Dayanna Claudino <day_claudino@hotmail.com>
2024-10-09 17:32:56 -0300

    feat: creates Hero component UI

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import NavBar from "../../components/NavBar/NavBar"
 import Hero from "./sections/Hero/Hero"
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
       <>
 
           <Hero/>
+          <NavBar />
           
       </>
     )
--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,19 +1,25 @@
-import { Button, Container, styled, Typography } from "@mui/material"
+import { Box, Container, styled, Typography } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.jpg"
 import Grid from '@mui/material/Grid2';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import theme from "../../../../theme";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackGround";
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(() => ({
-          backgroundColor: "black",
-          height: "100vh"
+    const StyledHero = styled("div")(({theme}) => ({
+          backgroundColor: theme.palette.primary.main,
+          height: "100vh",
+          display:"flex",
+          alignItems:"center"
     }))
 
     const StyledImg = styled("img")(() => ({
-        width: "100%",
-        borderRadius: "50%"
+        width: "80%",
+        borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
     }))
  
     return (
@@ -22,22 +28,35 @@ const Hero = () => {
         <StyledHero>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid size={{ xs: 12, md: 4 }}>
-                        <StyledImg src={Avatar}/>
+                    <Grid size={{ xs: 12, md: 5 }}>
+                        <Box position="relative">
+                            <Box position="absolute" width={"150%"} top={-100} >
+                                <AnimatedBackground/>
+                            </Box>
+                            <Box position="relative" textAlign="center">
+                                <StyledImg src={Avatar}/>
+                            </Box>
+                        </Box>
                     </Grid>
-                    <Grid size={{ xs: 12, md: 8 }}>
-                        <Typography color="primary" variant="h1" textAlign="center">Dayanna Claudino</Typography>
-                        <Typography color="primary" variant="h2" textAlign="center">Eu sou Analista de Sistemas e Suporte </Typography>
-                    <Grid container display="flex" justifyContent="center"> 
+                    <Grid size={{ xs: 12, md: 7 }}>
+                        <Typography color="primary.contrastText" variant="h2" textAlign="center" pb={2}>Dayanna Claudino</Typography>
+                        <Typography color="primary.contrastText" variant="h3" textAlign="center">Eu sou Analista de Sistemas </Typography>
+                    <Grid container display="flex" justifyContent="center" spacing={3} pt={3}> 
                         <Grid size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
-                            <Button variant="outlined">
-                            <FileDownloadIcon/>
-                            Download CV</Button>
+                            <StyledButton>
+                                <FileDownloadIcon/>
+                                    <Typography>
+                                        Download CV
+                                    </Typography>
+                            </StyledButton>
                         </Grid>
                         <Grid size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
-                            <Button variant="outlined">
+                            <StyledButton> 
                             <EmailIcon/>
-                            Contate-me</Button>
+                                <Typography>
+                                    Contate-me
+                                </Typography>
+                            </StyledButton>
                         </Grid>
                     </Grid>
                     </Grid>
